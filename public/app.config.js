a182af7d9ea52817a0756c3fa4379d88cc31470d
Nathan Aplin <solariousz@gmail.com>
2017-04-21 15:40:51 +0800

    Changed application structure in public

--- public/app.config.js
+++ public/app.config.js
@@ -10,53 +10,53 @@ config.$inject = ['$locationProvider', '$routeProvider'];
 function config($locationProvider, $routeProvider) {
 	$routeProvider
 	.when('/', {
-		templateUrl: 'views/home.html',
+		templateUrl: 'main/home.html',
 		restricted: false
 	})
 	.when('/words', {
-		templateUrl: 'views/wordsIndex.html',
+		templateUrl: 'main/wordsIndex.html',
 		controller: 'WordsIndexController',
 		controllerAs: 'vm',
 		restricted: false
 	})
 	.when('/words/:orcish', {
-		templateUrl: 'views/word.html',
+		templateUrl: 'main/word.html',
 		controller: 'WordController',
 		controllerAs: 'vm',
 		restricted: false
 	})
 	.when('/login', {
-		templateUrl: 'views/login.html',
+		templateUrl: 'admin/login.html',
 		controller: 'LoginController',
 		controllerAs: 'vm',
 		restricted: false
 	})
 	.when('/admin', {
-		templateUrl: 'views/admin.html',
+		templateUrl: 'admin/admin.html',
 		controller: 'AdminController',
 		controllerAs: 'vm',
 		restricted: true
 	})
 	.when('/admin/newword', {
-		templateUrl: 'views/newWord.html',
+		templateUrl: 'admin/newWord.html',
 		controller: 'NewWordController',
 		controllerAs: 'vm',
 		restricted: true
 	})
 	.when('/admin/bulkadd', {
-		templateUrl: 'views/bulkAdd.html',
+		templateUrl: 'admin/bulkAdd.html',
 		controller: 'BulkAddController',
 		controllerAs: 'vm',
 		restricted: true
 	})
 	.when('/admin/words/:orcish', {
-		templateUrl: 'views/adminWord.html',
+		templateUrl: 'admin/adminWord.html',
 		controller: 'AdminWordController',
 		controllerAs: 'vm',
 		restricted: true
 	})
 	.when('/search', {
-		templateUrl: 'views/search.html',
+		templateUrl: 'main/search.html',
 		controller: 'SearchController',
 		controllerAs: 'vm',
 		restricted: false
