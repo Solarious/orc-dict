4941be12f8c7179f089ea9ba93bfbd56af628354
Nathan Aplin <solariousz@gmail.com>
2017-05-04 17:49:47 +0800

    Removed testing buttons from admin page

--- public/admin/admin.controller.js
+++ public/admin/admin.controller.js
@@ -9,34 +9,6 @@ AdminController.$inject = ['$http', '$location', 'AlertService'];
 
 function AdminController($http, $location, AlertService) {
 	var vm = this;
-
-	vm.csrftest = csrftest;
-	vm.alertTest = alertTest;
-	vm.successTest = successTest;
-	vm.alertLinkTest = alertLinkTest;
-
-	function csrftest() {
-		console.log('Testing');
-		$http.post('/api/csrftest')
-		.then(function(response) {
-			console.log(response.data);
-		}, function(response) {
-			console.log(response);
-		});
-	}
-
-	function alertTest() {
-		AlertService.error('WARNING!!!');
-	}
-
-	function successTest() {
-		AlertService.success('SUCCESS!!!');
-	}
-
-	function alertLinkTest() {
-		$location.path('/');
-		AlertService.successDeferred('HUZZAH!!!');
-	}
 }
 
 })();
