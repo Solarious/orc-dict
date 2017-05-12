b94e3a610e40eaca2881a68388280ac6b1ea762f
Nathan Aplin <solariousz@gmail.com>
2017-05-12 23:40:21 +0800

    Added words to test/exampleWords.js

--- test/exampleWords.js
+++ test/exampleWords.js
@@ -32,6 +32,470 @@ function getWords() {
 					plural: 'orkors'
 				}
 			}
-		}
+		},
+		{
+			orcish: 'arkad',
+			english: 'magic',
+			PoS: 'noun',
+			noun: {
+				declension: 'first',
+				gender: 'feminine',
+				nominative: {
+					singular: 'arkad',
+					plural: 'arkadz'
+				},
+				genitive: {
+					singular: 'arkar',
+					plural: 'arkarru'
+				},
+				dative: {
+					singular: 'arkae',
+					plural: 'arkaes'
+				},
+				accusative: {
+					singular: 'arkaz',
+					plural: 'arkaruz'
+				},
+				vocative: {
+					singular: 'arkae',
+					plural: 'arkaes'
+				}
+			}
+		},
+		{
+			orcish: 'skuad',
+			english: 'sun',
+			PoS: 'noun',
+			noun: {
+				declension: 'first',
+				gender: 'feminine',
+				nominative: {
+					singular: 'skuad',
+					plural: 'skuadz'
+				},
+				genitive: {
+					singular: 'skuar',
+					plural: 'skuarru'
+				},
+				dative: {
+					singular: 'skuae',
+					plural: 'skuaes'
+				},
+				accusative: {
+					singular: 'skuaz',
+					plural: 'skuaruz'
+				},
+				vocative: {
+					singular: 'skuae',
+					plural: 'skuaes'
+				}
+			}
+		},
+		{
+			orcish: 'sagam',
+			english: 'universe',
+			PoS: 'noun',
+			noun: {
+				declension: 'first',
+				gender: 'feminine',
+				nominative: {
+					singular: 'sagam',
+					plural: 'sagadz'
+				},
+				genitive: {
+					singular: 'sagar',
+					plural: 'sagarru'
+				},
+				dative: {
+					singular: 'sagae',
+					plural: 'sagaes'
+				},
+				accusative: {
+					singular: 'sagadz',
+					plural: 'sagaruz'
+				},
+				vocative: {
+					singular: 'sagae',
+					plural: 'sagaes'
+				}
+			}
+		},
+		{
+			orcish: 'marsiam',
+			english: 'heresy',
+			PoS: 'noun',
+			noun: {
+				declension: 'first',
+				gender: 'feminine',
+				nominative: {
+					singular: 'marsiam',
+					plural: 'marsiadz'
+				},
+				genitive: {
+					singular: 'marsiar',
+					plural: 'marsiarru'
+				},
+				dative: {
+					singular: 'marsiae',
+					plural: 'marsiaes'
+				},
+				accusative: {
+					singular: 'marsiadz',
+					plural: 'marsiaruz'
+				},
+				vocative: {
+					singular: 'marsiae',
+					plural: 'marsiaes'
+				}
+			}
+		},
+		{
+			orcish: 'paizag',
+			english: 'sculptor',
+			PoS: 'noun',
+			noun: {
+				declension: 'first',
+				gender: 'feminine',
+				nominative: {
+					singular: 'paizag',
+					plural: 'paizadz'
+				},
+				genitive: {
+					singular: 'paizar',
+					plural: 'paizarru'
+				},
+				dative: {
+					singular: 'paizae',
+					plural: 'paizaes'
+				},
+				accusative: {
+					singular: 'paizaz',
+					plural: 'paizaruz'
+				},
+				vocative: {
+					singular: 'paizae',
+					plural: 'paizaes'
+				}
+			}
+		},
+		{
+			orcish: 'taeag',
+			english: 'crow',
+			PoS: 'noun',
+			noun: {
+				declension: 'first',
+				gender: 'feminine',
+				nominative: {
+					singular: 'taeag',
+					plural: 'taeadz'
+				},
+				genitive: {
+					singular: 'taear',
+					plural: 'taearru'
+				},
+				dative: {
+					singular: 'taeae',
+					plural: 'taeaes'
+				},
+				accusative: {
+					singular: 'taeaz',
+					plural: 'taearuz'
+				},
+				vocative: {
+					singular: 'taeae',
+					plural: 'taeaes'
+				}
+			}
+		},
+		{
+			orcish: 'gudul',
+			english: 'number',
+			PoS: 'noun',
+			noun: {
+				declension: 'second',
+				gender: 'masculine',
+				nominative: {
+					singular: 'gudul',
+					plural: 'gudulz'
+				},
+				genitive: {
+					singular: 'gudu',
+					plural: 'gudurru'
+				},
+				dative: {
+					singular: 'gudo',
+					plural: 'gudors'
+				},
+				accusative: {
+					singular: 'gududz',
+					plural: 'guduluz'
+				},
+				vocative: {
+					singular: 'gudo',
+					plural: 'gudors'
+				}
+			}
+		},
+		{
+			orcish: 'kaiul',
+			english: 'ally',
+			PoS: 'noun',
+			noun: {
+				declension: 'second',
+				gender: 'masculine',
+				nominative: {
+					singular: 'kaiul',
+					plural: 'kaiulz'
+				},
+				genitive: {
+					singular: 'kaiu',
+					plural: 'kaiurru'
+				},
+				dative: {
+					singular: 'kaio',
+					plural: 'kaiors'
+				},
+				accusative: {
+					singular: 'kaiudz',
+					plural: 'kaiuluz'
+				},
+				vocative: {
+					singular: 'kaio',
+					plural: 'kaiors'
+				}
+			}
+		},
+		{
+			orcish: 'ankor',
+			english: 'holiday',
+			PoS: 'noun',
+			noun: {
+				declension: 'second',
+				gender: 'masculine',
+				nominative: {
+					singular: 'ankor',
+					plural: 'ankulz'
+				},
+				genitive: {
+					singular: 'anku',
+					plural: 'ankurru'
+				},
+				dative: {
+					singular: 'anko',
+					plural: 'ankors'
+				},
+				accusative: {
+					singular: 'ankudz',
+					plural: 'ankuluz'
+				},
+				vocative: {
+					singular: 'anko',
+					plural: 'ankors'
+				}
+			}
+		},
+		{
+			orcish: 'djemaior',
+			english: 'lair',
+			PoS: 'noun',
+			noun: {
+				declension: 'second',
+				gender: 'masculine',
+				nominative: {
+					singular: 'djemaior',
+					plural: 'djemaiulz'
+				},
+				genitive: {
+					singular: 'djemaiu',
+					plural: 'djemaiurru'
+				},
+				dative: {
+					singular: 'djemaio',
+					plural: 'djemaiors'
+				},
+				accusative: {
+					singular: 'djemaiudz',
+					plural: 'djemaiuluz'
+				},
+				vocative: {
+					singular: 'djemaio',
+					plural: 'djemaiors'
+				}
+			}
+		},
+		{
+			orcish: 'aizak',
+			english: 'gravity',
+			PoS: 'noun',
+			noun: {
+				declension: 'second',
+				gender: 'masculine',
+				nominative: {
+					singular: 'aizak',
+					plural: 'aizulz'
+				},
+				genitive: {
+					singular: 'aizu',
+					plural: 'aizurru'
+				},
+				dative: {
+					singular: 'aizo',
+					plural: 'aizors'
+				},
+				accusative: {
+					singular: 'aizudz',
+					plural: 'aizuluz'
+				},
+				vocative: {
+					singular: 'aizo',
+					plural: 'aizors'
+				}
+			}
+		},
+		{
+			orcish: 'mesk',
+			english: 'face',
+			PoS: 'noun',
+			noun: {
+				declension: 'second',
+				gender: 'masculine',
+				nominative: {
+					singular: 'mesk',
+					plural: 'meskulz'
+				},
+				genitive: {
+					singular: 'mesku',
+					plural: 'meskurru'
+				},
+				dative: {
+					singular: 'mesko',
+					plural: 'meskors'
+				},
+				accusative: {
+					singular: 'meskudz',
+					plural: 'meskuluz'
+				},
+				vocative: {
+					singular: 'mesko',
+					plural: 'meskors'
+				}
+			}
+		},
+		{
+			orcish: 'shaidax',
+			english: 'problem',
+			PoS: 'noun',
+			noun: {
+				declension: 'second',
+				gender: 'masculine',
+				nominative: {
+					singular: 'shaidax',
+					plural: 'shaidulz'
+				},
+				genitive: {
+					singular: 'shaidu',
+					plural: 'shaidurru'
+				},
+				dative: {
+					singular: 'shaido',
+					plural: 'shaidors'
+				},
+				accusative: {
+					singular: 'shaidudz',
+					plural: 'shaiduluz'
+				},
+				vocative: {
+					singular: 'shaido',
+					plural: 'shaidors'
+				}
+			}
+		},
+		{
+			orcish: 'lorenx',
+			english: 'thunder',
+			PoS: 'noun',
+			noun: {
+				declension: 'second',
+				gender: 'masculine',
+				nominative: {
+					singular: 'lorenx',
+					plural: 'lorenulz'
+				},
+				genitive: {
+					singular: 'lorenu',
+					plural: 'lorenurru'
+				},
+				dative: {
+					singular: 'loreno',
+					plural: 'lorenors'
+				},
+				accusative: {
+					singular: 'lorenudz',
+					plural: 'lorenuluz'
+				},
+				vocative: {
+					singular: 'loreno',
+					plural: 'lorenors'
+				}
+			}
+		},
+		{
+			orcish: 'natalid',
+			english: 'blade',
+			PoS: 'noun',
+			noun: {
+				declension: 'second',
+				gender: 'neutral',
+				nominative: {
+					singular: 'natalid',
+					plural: 'natalidz'
+				},
+				genitive: {
+					singular: 'natalu',
+					plural: 'natalurru'
+				},
+				dative: {
+					singular: 'natalo',
+					plural: 'natalaes'
+				},
+				accusative: {
+					singular: 'nataludz',
+					plural: 'nataluluz'
+				},
+				vocative: {
+					singular: 'natalo',
+					plural: 'natalaes'
+				}
+			}
+		},
+		{
+			orcish: 'pakchuid',
+			english: 'nonsense',
+			PoS: 'noun',
+			noun: {
+				declension: 'second',
+				gender: 'neutral',
+				nominative: {
+					singular: 'pakchuid',
+					plural: 'pakchuidz'
+				},
+				genitive: {
+					singular: 'pakchuu',
+					plural: 'pakchuurru'
+				},
+				dative: {
+					singular: 'pakchuo',
+					plural: 'pakchuaes'
+				},
+				accusative: {
+					singular: 'pakchuudz',
+					plural: 'pakchuuluz'
+				},
+				vocative: {
+					singular: 'pakchuo',
+					plural: 'pakchuaes'
+				}
+			}
+		},
 	];
 }
