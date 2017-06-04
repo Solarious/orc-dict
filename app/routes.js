2fb2a8168505f0756fbbcc6eb34ae44c75b6998a
Nathan Aplin <solariousz@gmail.com>
2017-06-04 20:29:32 +0800

    Refactored app/routes.js
    module.exports now points to defined function rather than using an anonymous function
    The route definintions have been moved into addRouteForWords, addRoutesForUser, addRoutesForSearch, addRoutesForAutofill, addRoutesForBulkAdd and addRoutesForStats

--- app/routes.js
+++ app/routes.js
@@ -5,11 +5,27 @@ var passport = require('passport');
 var crypto = require('crypto');
 var Word = require('./models/word');
 var User = require('./models/user');
+var Sentence = require('./models/sentence');
 var autofill = require('./autofill');
 var bulkAdd = require('./bulkAdd');
 var search = require('./search');
 var email = require('./email');
 var stats = require('./stats');
+
+module.exports = addRoutes;
+
+function addRoutes(app) {
+	addRoutesForWords(app);
+	addRoutesForUser(app);
+	addRoutesForSearch(app);
+	addRoutesForAutofill(app);
+	addRoutesForBulkAdd(app);
+	addRoutesForStats(app);
+
+	app.get('(?!/api/)*', function(req, res) {
+		res.sendFile(path.resolve(__dirname, '../public/index.html'));
+	});
+}
 
 function updateWordFromReq(word, req) {
 	word.orcish = req.body.orcish;
@@ -64,373 +80,380 @@ function rebuild() {
 	});
 }
 
-module.exports = function(app) {
-
-app.get('/api/words', function(req, res) {
-	var resQuery = req.query || {};
-	var sort = resQuery.sort;
-	var skip = Number(resQuery.skip);
-	var limit = Number(resQuery.limit);
-	var getcount = resQuery.getcount;
-	var pos = resQuery.pos;
-	var query = Word.find({}).select({
-		orcish: 1,
-		english: 1,
-		PoS: 1,
-		num: 1
-	});
-	if (sort) {
-		query = query.sort(sort);
-	}
-	if (skip) {
-		query = query.skip(skip);
-	}
-	if (limit) {
-		query = query.limit(limit);
-	}
-	if (pos) {
-		query = query.where('PoS', pos);
-	}
-	var promises = [query.exec()];
-	if (getcount) {
-		if (pos) {
-			promises.push(Word.count().where('PoS', pos).exec());
-		} else {
-			promises.push(Word.count({}).exec());
-		}
-	}
-	Promise.all(promises)
-	.then(function(values) {
-		var data = {
-			words: values[0]
-		};
-		if (values[1]) {
-			data.count = values[1];
-		}
-		res.setHeader('Cache-Control', 'no-cache');
-		res.json(data);
-	}, function(error) {
-		res.status(500).send(error.message);
-	});
-});
-
-app.post('/api/words', function(req, res) {
-	if (!req.isAuthenticated()) {
-		res.status(401).send('Unauthorized');
-	} else {
-		var newWord = new Word();
-		updateWordFromReq(newWord, req);
-		newWord.save(function(err) {
-			if (err) {
-				res.status(500).send(getBetterErrorMessage(err));
-			} else {
-				res.json(newWord);
-				search.forCreate(newWord)
-				.catch(function(error) {
-					console.log('error with search.forCreate:');
-					console.log(error);
-				});
-			}
+function addRoutesForWords(app) {
+	app.get('/api/words', function(req, res) {
+		var resQuery = req.query || {};
+		var sort = resQuery.sort;
+		var skip = Number(resQuery.skip);
+		var limit = Number(resQuery.limit);
+		var getcount = resQuery.getcount;
+		var pos = resQuery.pos;
+		var query = Word.find({}).select({
+			orcish: 1,
+			english: 1,
+			PoS: 1,
+			num: 1
 		});
-	}
-});
-
-app.get('/api/words/:orcish/:num', function(req, res) {
-	Word.findOne({
-		'orcish': req.params.orcish,
-		'num': req.params.num
-	}, function(err, word) {
-		if (err) {
-			res.status(500).send(err.message);
-		} else {
-			if (!word) {
-				var wordStr = req.params.orcish + ' ' + req.params.num;
-				res.status(404).send(
-					'cannot find word: ' + wordStr
-				);
+		if (sort) {
+			query = query.sort(sort);
+		}
+		if (skip) {
+			query = query.skip(skip);
+		}
+		if (limit) {
+			query = query.limit(limit);
+		}
+		if (pos) {
+			query = query.where('PoS', pos);
+		}
+		var promises = [query.exec()];
+		if (getcount) {
+			if (pos) {
+				promises.push(Word.count().where('PoS', pos).exec());
 			} else {
-				res.setHeader('Cache-Control', 'no-cache');
-				res.json(word);
+				promises.push(Word.count({}).exec());
 			}
 		}
+		Promise.all(promises)
+		.then(function(values) {
+			var data = {
+				words: values[0]
+			};
+			if (values[1]) {
+				data.count = values[1];
+			}
+			res.setHeader('Cache-Control', 'no-cache');
+			res.json(data);
+		}, function(error) {
+			res.status(500).send(error.message);
+		});
 	});
-});
 
-app.put('/api/words/:orcish/:num', function(req, res) {
-	if (!req.isAuthenticated()) {
-		return res.status(401).send('Unauthorized');
-	} else {
+	app.post('/api/words', function(req, res) {
+		if (!req.isAuthenticated()) {
+			res.status(401).send('Unauthorized');
+		} else {
+			var newWord = new Word();
+			updateWordFromReq(newWord, req);
+			newWord.save(function(err) {
+				if (err) {
+					res.status(500).send(getBetterErrorMessage(err));
+				} else {
+					res.json(newWord);
+					search.forCreate(newWord)
+					.catch(function(error) {
+						console.log('error with search.forCreate:');
+						console.log(error);
+					});
+				}
+			});
+		}
+	});
+
+	app.get('/api/words/:orcish/:num', function(req, res) {
 		Word.findOne({
 			'orcish': req.params.orcish,
 			'num': req.params.num
 		}, function(err, word) {
 			if (err) {
-				return res.status(500).send(err.message);
+				res.status(500).send(err.message);
+			} else {
+				if (!word) {
+					var wordStr = req.params.orcish + ' ' + req.params.num;
+					res.status(404).send(
+						'cannot find word: ' + wordStr
+					);
+				} else {
+					res.setHeader('Cache-Control', 'no-cache');
+					res.json(word);
+				}
 			}
-			word = word || new Word();
-			updateWordFromReq(word, req);
-			if (req.params.orcish !== word.orcish) {
-				word.num = undefined;
-			}
-			word.save(function(err) {
+		});
+	});
+
+	app.put('/api/words/:orcish/:num', function(req, res) {
+		if (!req.isAuthenticated()) {
+			return res.status(401).send('Unauthorized');
+		} else {
+			Word.findOne({
+				'orcish': req.params.orcish,
+				'num': req.params.num
+			}, function(err, word) {
 				if (err) {
-					res.status(500).send(getBetterErrorMessage(err));
+					return res.status(500).send(err.message);
+				}
+				word = word || new Word();
+				updateWordFromReq(word, req);
+				if (req.params.orcish !== word.orcish) {
+					word.num = undefined;
+				}
+				word.save(function(err) {
+					if (err) {
+						res.status(500).send(getBetterErrorMessage(err));
+					} else {
+						res.json(word);
+						search.forUpdate(
+							req.params.orcish,
+							req.params.num,
+							word
+						).catch(function(error) {
+							console.log('error with search.forUpdate:');
+							console.log(error);
+						});
+					}
+				});
+			});
+		}
+	});
+
+	app.delete('/api/words/:orcish/:num', function(req, res) {
+		if (!req.isAuthenticated()) {
+			res.status(401).send('Unauthorized');
+		} else {
+			Word.findOneAndRemove({
+				'orcish': req.params.orcish,
+				'num': req.params.num
+			}, function (err, word) {
+				if (err) {
+					res.status(500).send(err.message);
+				} else if (!word) {
+					var wordStr = req.params.orcish + ' ' + req.params.num;
+					res.status(404).send('word ' + wordStr + ' does not exits');
 				} else {
 					res.json(word);
-					search.forUpdate(req.params.orcish, req.params.num, word)
+					search.forRemove(word)
 					.catch(function(error) {
-						console.log('error with search.forUpdate:');
+						console.log('error with search.forRemove:');
 						console.log(error);
 					});
 				}
 			});
-		});
-	}
-});
+		}
+	});
 
-app.delete('/api/words/:orcish/:num', function(req, res) {
-	if (!req.isAuthenticated()) {
-		res.status(401).send('Unauthorized');
-	} else {
-		Word.findOneAndRemove({
-			'orcish': req.params.orcish,
-			'num': req.params.num
-		}, function (err, word) {
-			if (err) {
-				res.status(500).send(err.message);
-			} else if (!word) {
-				var wordStr = req.params.orcish + ' ' + req.params.num;
-				res.status(404).send('word ' + wordStr + ' does not exits');
-			} else {
-				res.json(word);
-				search.forRemove(word)
-				.catch(function(error) {
-					console.log('error with search.forRemove:');
-					console.log(error);
+	app.delete('/api/words-by-pos/:pos', function(req, res) {
+		var PoS = req.params.pos;
+		var query;
+		if (PoS === 'all') {
+			query = Word.remove({});
+		} else {
+			let enums = Word.schema.path('PoS').enumValues;
+			if (Word.schema.path('PoS').enumValues.indexOf(PoS) !== -1) {
+				query = Word.remove({
+					PoS: PoS
 				});
-			}
-		});
-	}
-});
-
-app.get('/api/autofillword/:PoS/:orcish', function(req, res) {
-	var PoS = req.params.PoS;
-	var orcish = req.params.orcish;
-	autofill(orcish, PoS, function(err, wordPart) {
-		if (err) {
-			res.status(404).send(err.message);
-		} else {
-			res.json(wordPart);
-		}
-	});
-});
-
-app.post('/api/user/login', function(req, res, next) {
-	passport.authenticate('local', function(err, user, info) {
-		if (err) {
-			return next(err);
-		}
-		if (!user) {
-			return res.status(401).send(info.message);
-		}
-		req.logIn(user, function(err) {
-			if (err) {
-				return res.status(500).send(err.message);
-			}
-			res.status(200).json({
-				status: 'Login successful'
-			});
-		});
-	})(req, res, next);
-});
-
-app.post('/api/user/logout', function(req, res) {
-	req.logout();
-	res.json({
-		status: 'Logout success'
-	});
-});
-
-app.get('/api/user/status', function(req, res) {
-	if (req.isAuthenticated()) {
-		return res.status(200).json({
-			status: true,
-			username: req.user.username
-		});
-	} else {
-		return res.status(200).json({
-			status: false
-		});
-	}
-});
-
-app.post('/api/user/forgot', function(req, res) {
-	var token;
-
-	new Promise(function(resolve, reject) {
-		crypto.randomBytes(20, function(err, buf) {
-			if (err) {
-				reject(err);
 			} else {
-				resolve(buf.toString('hex'));
+				return res.status(400).send('Invalid PoS ' + PoS);
 			}
-		});
-	})
-	.then(function(data) {
-		token = data;
-		return User.findOne({
-			email: req.body.email
-		}).exec();
-	})
-	.then(function(user) {
-		if (!user) {
-			throw 'No account with that email address exists';
 		}
-
-		user.resetPasswordToken = token;
-		user.resetPasswordExpires = Date.now() + 3600000;
-		return user.save();
-	})
-	.then(function(user) {
-		return email.sendReset(user.email, token, req.headers.host);
-	})
-	.then(function(response) {
-		res.send('Reset email successfully sent');
-	})
-	.catch(function(error) {
-		console.log(error);
-		res.status(500).send(error.message);
-	});
-});
-
-app.post('/api/user/reset/', function(req, res) {
-	var token = req.body.token;
-	var password = req.body.password;
-
-	User.findOne({
-		resetPasswordToken: token,
-		resetPasswordExpires: {
-			$gt: Date.now()
-		}
-	}).exec()
-	.then(function(user) {
-		if (!user) {
-			throw 'Password reset token is invalid or has expired';
-		} else {
-			user.password = password;
-			user.resetPasswordToken = undefined;
-			user.resetPasswordExpires = undefined;
-			return user.save();
-		}
-	})
-	.then(function(user) {
-		return res.send('Password successfully reset');
-	})
-	.catch(function(error) {
-		var message = error.message || error;
-		return res.status(400).send(message);
-	});
-});
-
-app.post('/api/csrftest', function(req, res) {
-	res.json({ message: 'CSRF test success' });
-});
-
-app.post('/api/bulkadd', function(req, res) {
-	if (!req.isAuthenticated()) {
-		res.status(401).send('Unauthorized');
-	} else {
-		var data = req.body.data;
-		var encoding = req.body.encoding;
-		var updateMethod = req.body.updateMethod;
-		var order = req.body.order;
-		bulkAdd(data, encoding, updateMethod, order)
-		.then(function(results) {
-			res.json(results);
+		query.exec()
+		.then(function(result) {
+			return res.json(result);
 		})
 		.catch(function(error) {
-			res.status(404).send(error.message);
+			res.status(500).send(error.message);
 		});
-	}
-});
+	});
+}
 
-app.get('/api/search', function(req, res) {
-	var query = req.query || {};
-	var q = query.q;
-	if (!q) {
-		return res.status(400).send('missing query paramerter q');
-	}
+function addRoutesForUser(app) {
+	app.post('/api/user/login', function(req, res, next) {
+		passport.authenticate('local', function(err, user, info) {
+			if (err) {
+				return next(err);
+			}
+			if (!user) {
+				return res.status(401).send(info.message);
+			}
+			req.logIn(user, function(err) {
+				if (err) {
+					return res.status(500).send(err.message);
+				}
+				res.status(200).json({
+					status: 'Login successful'
+				});
+			});
+		})(req, res, next);
+	});
 
-	search.getMatches(q)
-	.then(function(data) {
+	app.post('/api/user/logout', function(req, res) {
+		req.logout();
 		res.json({
-			results: data
+			status: 'Logout success'
 		});
-	})
-	.catch(function(error) {
-		res.status(500).send(error.message);
 	});
-});
 
-app.post('/api/search/rebuild', function(req, res) {
-	search.rebuild()
-	.catch(function(error) {
-		console.log('error with search.rebuild');
-		console.log(error);
-	});
-	res.send('Search Indexes are now being rebuilt');
-});
-
-app.get('/api/list-search-indexes', function(req, res) {
-	search.getAll(function(error, data) {
-		if (error) {
-			res.status(500).send(error.message);
-		} else {
-			res.send(data);
-		}
-	});
-});
-
-app.post('/api/stats', function(req, res) {
-	if (!req.isAuthenticated()) {
-		res.status(401).send('Unauthorized');
-	} else {
-		stats.get()
-		.then(function(stats) {
-			res.json(stats);
-		})
-		.catch(function(error) {
-			res.status(500).send(error.message);
-		});
-	}
-});
-
-app.delete('/api/words-by-pos/:pos', function(req, res) {
-	var PoS = req.params.pos;
-	var query;
-	if (PoS === 'all') {
-		query = Word.remove({});
-	} else {
-		let enums = Word.schema.path('PoS').enumValues;
-		if (Word.schema.path('PoS').enumValues.indexOf(PoS) !== -1) {
-			query = Word.remove({
-				PoS: PoS
+	app.get('/api/user/status', function(req, res) {
+		if (req.isAuthenticated()) {
+			return res.status(200).json({
+				status: true,
+				username: req.user.username
 			});
 		} else {
-			return res.status(400).send('Invalid PoS ' + PoS);
+			return res.status(200).json({
+				status: false
+			});
 		}
-	}
-	query.exec()
-	.then(function(result) {
-		return res.json(result);
-	})
-	.catch(function(error) {
-		res.status(500).send(error.message);
 	});
-});
 
-app.get('(?!/api/)*', function(req, res) {
-	res.sendFile(path.resolve(__dirname, '../public/index.html'));
-});
+	app.post('/api/user/forgot', function(req, res) {
+		var token;
 
-};// module.exports
+		new Promise(function(resolve, reject) {
+			crypto.randomBytes(20, function(err, buf) {
+				if (err) {
+					reject(err);
+				} else {
+					resolve(buf.toString('hex'));
+				}
+			});
+		})
+		.then(function(data) {
+			token = data;
+			return User.findOne({
+				email: req.body.email
+			}).exec();
+		})
+		.then(function(user) {
+			if (!user) {
+				throw 'No account with that email address exists';
+			}
+
+			user.resetPasswordToken = token;
+			user.resetPasswordExpires = Date.now() + 3600000;
+			return user.save();
+		})
+		.then(function(user) {
+			return email.sendReset(user.email, token, req.headers.host);
+		})
+		.then(function(response) {
+			res.send('Reset email successfully sent');
+		})
+		.catch(function(error) {
+			console.log(error);
+			res.status(500).send(error.message);
+		});
+	});
+
+	app.post('/api/user/reset/', function(req, res) {
+		var token = req.body.token;
+		var password = req.body.password;
+
+		User.findOne({
+			resetPasswordToken: token,
+			resetPasswordExpires: {
+				$gt: Date.now()
+			}
+		}).exec()
+		.then(function(user) {
+			if (!user) {
+				throw 'Password reset token is invalid or has expired';
+			} else {
+				user.password = password;
+				user.resetPasswordToken = undefined;
+				user.resetPasswordExpires = undefined;
+				return user.save();
+			}
+		})
+		.then(function(user) {
+			return res.send('Password successfully reset');
+		})
+		.catch(function(error) {
+			var message = error.message || error;
+			return res.status(400).send(message);
+		});
+	});
+
+	app.post('/api/csrftest', function(req, res) {
+		res.json({ message: 'CSRF test success' });
+	});
+}
+
+function addRoutesForSearch(app) {
+	app.get('/api/search', function(req, res) {
+		var query = req.query || {};
+		var q = query.q;
+		if (!q) {
+			return res.status(400).send('missing query paramerter q');
+		}
+
+		search.getMatches(q)
+		.then(function(data) {
+			res.json({
+				results: data
+			});
+		})
+		.catch(function(error) {
+			res.status(500).send(error.message);
+		});
+	});
+
+	app.post('/api/search/rebuild', function(req, res) {
+		search.rebuild()
+		.catch(function(error) {
+			console.log('error with search.rebuild');
+			console.log(error);
+		});
+		res.send('Search Indexes are now being rebuilt');
+	});
+
+	app.get('/api/list-search-indexes', function(req, res) {
+		search.getAll(function(error, data) {
+			if (error) {
+				res.status(500).send(error.message);
+			} else {
+				res.send(data);
+			}
+		});
+	});
+}
+
+function addRoutesForAutofill(app) {
+	app.get('/api/autofillword/:PoS/:orcish', function(req, res) {
+		var PoS = req.params.PoS;
+		var orcish = req.params.orcish;
+		autofill(orcish, PoS, function(err, wordPart) {
+			if (err) {
+				res.status(404).send(err.message);
+			} else {
+				res.json(wordPart);
+			}
+		});
+	});
+}
+
+function addRoutesForBulkAdd(app) {
+	app.post('/api/bulkadd', function(req, res) {
+		if (!req.isAuthenticated()) {
+			res.status(401).send('Unauthorized');
+		} else {
+			var data = req.body.data;
+			var encoding = req.body.encoding;
+			var updateMethod = req.body.updateMethod;
+			var order = req.body.order;
+			bulkAdd(data, encoding, updateMethod, order)
+			.then(function(results) {
+				res.json(results);
+			})
+			.catch(function(error) {
+				res.status(404).send(error.message);
+			});
+		}
+	});
+}
+
+function addRoutesForStats(app) {
+	app.post('/api/stats', function(req, res) {
+		if (!req.isAuthenticated()) {
+			res.status(401).send('Unauthorized');
+		} else {
+			stats.get()
+			.then(function(stats) {
+				res.json(stats);
+			})
+			.catch(function(error) {
+				res.status(500).send(error.message);
+			});
+		}
+	});
+}
