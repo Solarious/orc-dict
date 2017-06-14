84bf72485d9e09ceb238e1a6f41476f8e7ecbd67
Nathan Aplin <solariousz@gmail.com>
2017-06-14 15:40:53 +0800

    Removed method-override and gulp-rename

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
-var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
 if (process.env.NODE_ENV !== 'production') {
 	var jshint = require('gulp-jshint');
