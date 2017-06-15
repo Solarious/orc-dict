4ab7975dea55e2500df9db1880f8ac9e86aea45a
Nathan Aplin <solariousz@gmail.com>
2017-06-15 22:35:20 +0800

    Improved content in editHelpModal

--- public/src/editable/editHelpModal.directive.js
+++ public/src/editable/editHelpModal.directive.js
@@ -11,10 +11,25 @@ function editHelpModal() {
 		scope: {
 			modalId: '@'
 		},
-		templateUrl: 'src/editable/editHelpModal.directive.html'
+		templateUrl: 'src/editable/editHelpModal.directive.html',
+		controller: EditHelpModalController,
+		controllerAs: 'vm',
+		bindToController: true
 	};
 
 	return directive;
+}
+
+EditHelpModalController.$inject = ['$location'];
+
+function EditHelpModalController($location) {
+	var vm = this;
+
+	vm.getHost = getHost;
+
+	function getHost() {
+		return $location.host();
+	}
 }
 
 })();
