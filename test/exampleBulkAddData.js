2509f466ad3c7f1f4a683281ae1fe09817c3d171
Nathan Aplin <solariousz@gmail.com>
2017-07-19 19:40:29 +0800

    Changed data in test/exampleBulkAddData.js to fit copular verb changes
    Because bulkAdd no longer automatically throws an error with copular verbs, 'c.i.' is used to cause word.copula.gerund.declension to have an invalid value, and thus cause a validation error

--- test/exampleBulkAddData.js
+++ test/exampleBulkAddData.js
@@ -433,7 +433,9 @@ function errorDataCsvEop() {
 	a += '"six","grae","cardinal"\n';
 	a += '"seven","milz","cardinal"\n';
 	a += '"eight","roi","cardinal"\n';
-	a += '"me","nudz","copular verb"\n';
+	a += '"me","nudz","copular verb","c.i.",';
+	a += '"1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23';
+	a += ' 24 25 26 27 28 29 30 31 32"\n';
 	a += '"nine","gluk","cardinal"\n';
 	a += '"ten","lotta","cardinal"\n';
 	a += '"eleven","ked","cardinal"\n';
